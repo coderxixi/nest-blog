508e2f58dae4b39f89bceb1ecd6d9a000e508a70
liuss7 <liuss7@asiainfo.com>
2023-03-15 21:33:38 +0800

    up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('a')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('b')
   getHello(): string {
     return this.appService.getHello();
   }
